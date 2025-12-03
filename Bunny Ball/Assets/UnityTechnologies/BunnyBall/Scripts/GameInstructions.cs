575ae278c64d06d4073372926e01bbf3215adbbd
Henry Barcalow <henry.barcalow@gmail.com>
2025-12-03 13:19:29 -0500

    Add instructions

---
+++ Bunny Ball/Assets/UnityTechnologies/BunnyBall/Scripts/GameInstructions.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class GameInstructions : MonoBehaviour
+{
+    private string controls = "Move using the WASD keys";
+    void Start()
+    {
+        print(controls);
+    }
+
+    void Update()
+    {
+        
+    }
+}
